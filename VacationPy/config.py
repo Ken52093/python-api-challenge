3460b5de6cb1980212ebf504dcf1867906754b11
Ken Tseng <84150117+Ken52093@users.noreply.github.com>
2021-07-31 13:45:00 -0400

    Update config.py

--- VacationPy/config.py
+++ VacationPy/config.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "f5e9ad0559420da2ac66878e1c8a974b"
+weather_api_key = "YOUR_KEY"
 
 # Google API Key
-g_key = "AIzaSyAXnlHILhT5Vt07FmZhkuEMvYjy0viyQro"
+g_key = "YOUR_KEY"
